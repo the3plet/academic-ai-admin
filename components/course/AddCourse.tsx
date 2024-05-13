f5543b1203affbf0b9997933581dd1453da43c82
shinjith-dev <shinjithpallikkara@gmail.com>
2024-05-13 23:42:07 +0530

    fix: add course error

--- components/course/AddCourse.tsx
+++ components/course/AddCourse.tsx
@@ -1,67 +1,50 @@
-"use client"
-import { Button, Drawer, Stack, TagsInput, TextInput } from "@mantine/core"
-import { useDisclosure } from "@mantine/hooks"
-import axios from "axios"
-import React from "react"
-import { Controller, useForm } from "react-hook-form"
-import useToast from "../hooks/useToast"
-import { useAddCourse } from "@/lib/query-hooks/course"
+"use client";
+import { Button, Drawer, Stack, TagsInput, TextInput } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import axios from "axios";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import useToast from "../hooks/useToast";
+import { useAddCourse } from "@/lib/query-hooks/course";
 
-type Props = {}
+type Props = {};
 
 const defaultValues = {
   scheme: "2019",
   no_of_modules: "5",
-}
+};
+
+const topicFields = [1, 2, 3, 4, 5];
 
 const AddCourse = (props: Props) => {
-  const [opened, { open, close }] = useDisclosure(false)
-  const { Toast, showToast } = useToast()
-  const form = useForm<any>({ defaultValues })
-  const { mutate: addCourse } = useAddCourse()
+  const [opened, { open, close }] = useDisclosure(false);
+  const { Toast, showToast } = useToast();
+  const form = useForm<any>({ defaultValues });
+  const { mutate: addCourse } = useAddCourse();
 
   const onSubmit = (data: any) => {
     // showToast("Course saved", "success");
     const submitValues = {
       ...data,
       topics: [],
-      no_of_modules: Number(data.no_of_modules),
+      no_of_modules: 5,
       semester: Number(data.semester),
       hours: Number(data.hours),
       credits: Number(data.credits),
-    }
-    console.log(submitValues)
+    };
+    console.log(submitValues);
 
-    addCourse(data)
-  }
-
-  const getTopicFields = () => {
-    try {
-      if (Number(form.watch("no_of_modules"))) {
-        return [...Array(Number(form.watch("no_of_modules")))].map((field, index) => (
-          <Controller
-            name="course_code"
-            control={form.control}
-            render={({ field }) => (
-              <TagsInput
-                component="textarea"
-                {...field}
-                label={`Module ${index + 1}`}
-                description="Enter syllabus topics in this module; either by seperated by comma(,) or by using enter key after each topic"
-                placeholder="Enter syllabus topics"
-              />
-            )}
-          />
-        ))
-      }
-    } catch (e) {
-      return null
-    }
-  }
+    addCourse(data);
+  };
 
   return (
     <>
-      <Drawer opened={opened} onClose={close} title="Add Course" position="right">
+      <Drawer
+        opened={opened}
+        onClose={close}
+        title="Add Course"
+        position="right"
+      >
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <Stack gap="sm">
             <Controller
@@ -164,7 +147,23 @@ const AddCourse = (props: Props) => {
               )}
             />
 
-            {getTopicFields()}
+            {topicFields.map((_, index) => (
+              <Controller
+                key={`topics-field-${index}`}
+                name={`module-${index}-topic`}
+                control={form.control}
+                render={({ field }) => (
+                  <TagsInput
+                    component="textarea"
+                    {...field}
+                    label={`Module ${index + 1}`}
+                    description="Enter syllabus topics in this module; either by seperated by comma(,) or by using enter key after each topic"
+                    placeholder="Enter syllabus topics"
+                  />
+                )}
+              />
+            ))}
+
             <Button type="submit">Save</Button>
           </Stack>
         </form>
@@ -174,7 +173,7 @@ const AddCourse = (props: Props) => {
 
       {Toast && <Toast />}
     </>
-  )
-}
+  );
+};
 
-export default AddCourse
+export default AddCourse;
