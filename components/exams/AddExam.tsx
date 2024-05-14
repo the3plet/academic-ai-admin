a9ff19aea4315f6320c0470287ae9a5dacd728d3
shinjith-dev <shinjithpallikkara@gmail.com>
2024-05-14 13:00:56 +0530

    feat: safe commit

--- components/exams/AddExam.tsx
+++ components/exams/AddExam.tsx
@@ -1,33 +1,32 @@
 "use client";
 import { Controller, useForm } from "react-hook-form";
-import { Button, Modal, SimpleGrid, TextInput } from "@mantine/core";
+import { Button, Modal, SimpleGrid, Stack, TextInput } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import React from "react";
 import { useAddExams } from "@/lib/query-hooks/exams";
 import useToast from "../hooks/useToast";
-
 
 type Props = {};
 
 const AddExam = (props: Props) => {
   const [opened, { open, close }] = useDisclosure(false);
   const { Toast, showToast } = useToast();
-  const {mutate:addExam}= useAddExams();
+  const { mutate: addExam } = useAddExams();
   const form = useForm<any>();
- const onSubmit =(data:any)=>{
-    const submitValues ={
-        ...data,
-        department_name:String(data.department_name),
-        semester:String(data.semester),
-        series_one_date:String(data.series_one_date),
-        series_two_date:String(data.series_two_date),
-        end_semester_date:String(data.end_semester_date),
-        assignment_1_date:String(data.assignment_1_date),
-        assignement_2_date:String(data.assignment_2_date)
+  const onSubmit = (data: any) => {
+    const submitValues = {
+      ...data,
+      department_name: String(data.department_name),
+      semester: String(data.semester),
+      series_one_date: String(data.series_one_date),
+      series_two_date: String(data.series_two_date),
+      end_semester_date: String(data.end_semester_date),
+      assignment_1_date: String(data.assignment_1_date),
+      assignement_2_date: String(data.assignment_2_date),
     };
-    console.log(submitValues)
-    addExam(submitValues)
- }
+    console.log(submitValues);
+    addExam(submitValues);
+  };
   return (
     <>
       <Button onClick={open}>Add Exam</Button>
@@ -52,11 +51,7 @@ const AddExam = (props: Props) => {
                 name="semester"
                 control={form.control}
                 render={({ field }) => (
-                  <TextInput
-                    {...field}
-                    label="Semester"
-                    placeholder="2"
-                  />
+                  <TextInput {...field} label="Semester" placeholder="2" />
                 )}
               />
             </div>
@@ -68,7 +63,8 @@ const AddExam = (props: Props) => {
                   <TextInput
                     {...field}
                     label="Series I"
-                    placeholder="YYYY-MM-DD"                  />
+                    placeholder="YYYY-MM-DD"
+                  />
                 )}
               />
             </div>
@@ -80,7 +76,8 @@ const AddExam = (props: Props) => {
                   <TextInput
                     {...field}
                     label="Series II"
-                    placeholder="YYYY-MM-DD"                  />
+                    placeholder="YYYY-MM-DD"
+                  />
                 )}
               />
             </div>
@@ -92,7 +89,8 @@ const AddExam = (props: Props) => {
                   <TextInput
                     {...field}
                     label="Semester Exam"
-                    placeholder="YYYY-MM-DD"                  />
+                    placeholder="YYYY-MM-DD"
+                  />
                 )}
               />
             </div>
@@ -104,7 +102,8 @@ const AddExam = (props: Props) => {
                   <TextInput
                     {...field}
                     label="Assignment I"
-                    placeholder="YYYY-MM-DD"                  />
+                    placeholder="YYYY-MM-DD"
+                  />
                 )}
               />
             </div>
@@ -116,12 +115,17 @@ const AddExam = (props: Props) => {
                   <TextInput
                     {...field}
                     label="Assignment II"
-                    placeholder="YYYY-MM-DD"                  />
+                    placeholder="YYYY-MM-DD"
+                  />
                 )}
               />
             </div>
-            <Button type="submit">Submit</Button>
           </SimpleGrid>
+          <Stack justify="center">
+            <Button type="submit" mt={8}>
+              Submit
+            </Button>
+          </Stack>
         </form>
       </Modal>
       {Toast && <Toast />}
